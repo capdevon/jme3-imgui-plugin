042a7a3a92e658a1b581231cd56f2257b485bb81
capedvon <jefferydeaver2010@gmail.com>
2026-06-16 09:50:35 +0200

    ImGuiTheme: javadoc

--- src/main/java/com/jme3/imgui/ImGuiTheme.java
+++ src/main/java/com/jme3/imgui/ImGuiTheme.java
@@ -4,14 +4,33 @@ import imgui.ImGui;
 import imgui.ImGuiStyle;
 
 /**
+ * Utility class for managing and applying built-in ImGui themes .
+ *
  * @author capdevon
  */
-public class ImGuiTheme {
+public final class ImGuiTheme {
 
+    /**
+     * Supported visual themes for the ImGui interface.
+     */
     public enum Theme {
-        DARK, LIGHT, CLASSIC
+        /** Default dark theme. */
+        DARK,
+        /** Standard light theme. */
+        LIGHT,
+        /** Legacy ImGui classic theme. */
+        CLASSIC
     }
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private ImGuiTheme() {}
+
+    /**
+     * Applies the specified theme to the current ImGui context.
+     * * @param theme the {@link Theme} to apply
+     */
     public static void apply(Theme theme) {
         switch (theme) {
             case DARK:
